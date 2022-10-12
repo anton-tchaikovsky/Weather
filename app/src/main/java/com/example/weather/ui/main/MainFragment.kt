50b046500b9429fc92856f78c9b687b45574a245
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2022-10-12 17:44:41 +0300

    Первое MVVM.

--- app/src/main/java/com/example/weather/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/weather/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.weather.R
 
 class MainFragment : Fragment() {
@@ -23,10 +25,19 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        val observer = Observer<Any> { renderData (it) }
+        val liveDataToObserve =  viewModel.getLiveData()
+        liveDataToObserve.observe(viewLifecycleOwner, observer)
         // TODO: Use the ViewModel
     }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(context, "data", Toast.LENGTH_LONG).show()
+    }
+
 
 }
--- app/src/main/java/com/example/weather/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/weather/ui/main/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.weather.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel (private val liveDataToObserve: MutableLiveData<Any> =
+                         MutableLiveData()) : ViewModel() {
+    fun getLiveData(): LiveData<Any> {
+        getDataFromLocalSource()
+        return liveDataToObserve
+    }
+
+    private fun getDataFromLocalSource(){
+        Thread{
+            Thread.sleep(3000)
+            liveDataToObserve.postValue(Any())
+        }.start()
+    }
 }
