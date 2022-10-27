e014a43c2d5e4535e6c561d0f4978911b0bdaa82
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2022-10-27 14:38:19 +0300

    Создал активити-браузер, которая отображает web-страницы, вызвал ее из основной активити через неявный интент с передачей Uri.

--- app/src/main/java/com/example/weather/view/WeatherActivity.kt
+++ app/src/main/java/com/example/weather/view/WeatherActivity.kt
@@ -1,15 +1,29 @@
 package com.example.weather.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.example.weather.R
-import com.example.weather.databinding.WeatherActivityBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.databinding.WebViewBinding
 
 
 class WeatherActivity : AppCompatActivity() {
 
+    private lateinit var binding:WebViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = WebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ok.setOnClickListener{
+            startActivity(Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(binding.url.text.toString())
+             })
+        }
+
+/*
         // создание view соответствующего макета и его установка
         setContentView(WeatherActivityBinding.inflate(layoutInflater).root)
 
@@ -19,6 +33,7 @@ class WeatherActivity : AppCompatActivity() {
                 .replace(R.id.container, CityListFragment.newInstance())
                 .commitNow()
         }
+*/
     }
 
 }
